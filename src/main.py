366b1cb20ccc77b115de3d8662592defd3dc4b87
JoKeRooo7 <renal98@mail.ru>
2024-02-18 18:53:16 +0300

    update

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import json
+
+with open("wiki_file.json", "r+") as file:
+    json.dump(json.load(file), file, indent=4)
+
+
+
+dict = 
+{
+    lin1: {}
+    lin2: {
+        lin3:{}
+        lin4:{
+            link5:{}
+            link6:{
+                link5: {}
+                link6:{}
+                link5: {}
+                link6:{}
+                link5: {}
+                link6: {}
+                link5: {}
+                link6:{}
+                link5: {}
+                link6:{}
+                link5: {}
+                link6:{}
+                link5: {}
+                link6:{}
+            }
+        }
+    }
+
+}
